e90e536ef201a7beb09ce9dcbdf1bfa1f095ab62
Giacomo Carnovalini <giacomo.carnovalini@gmail.com>
2022-03-22 22:36:12 +0000

    final touches and changelog update

--- README.md
+++ README.md
@@ -9,12 +9,16 @@ Notifies you when your reviews and lessons are due. There's not much else to it.
 * A badge shows how many reviews are due.
 * Click on the extension to open a pop-up with links to your lessons and reviews.
 
+## NEW in version 2.0.1
+Reworked to be compliant with Chrome extensions manifest V3.
+Moved from timers to alarms, which limits API calls to one call per minute.
+Added option for vacation mode - this will hide your reviews count while on vacation.
+
+## NEW in version 1.1.3
+Improved UX/UI - thanks to [Ben Grant](https://github.com/GRA0007)!
+
 ## NEW in version 1.1.1
 Added a tickbox in the settings page to get politely reminded of your lessons as well.
 The box is unticked by deafult, so if you don't need the constant reminder there's no change for you.
 If you like the pressure however, you can go and tick the box.
-
 You'll get a badge with your lessons count ONLY if you're done with your reviews.
-
-## NEW in version 1.1.3
-Improved UX/UI - thanks to [Ben Grant](https://github.com/GRA0007)!
--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 // Check and store API token
-chrome.storage.sync.get("WKapikey", function(result) {
+chrome.storage.sync.get(["WKapikey", "vacationModeActive"], function(result) {
   var WKToken = result.WKapikey;
+  var onvacation = result.vacationModeActive;
 
   function apiEndpoint(path, token) {
     let requestHeaders =
@@ -20,30 +21,31 @@ chrome.storage.sync.get("WKapikey", function(result) {
     chrome.runtime.openOptionsPage();
 
   } else { // API Token is found in storage
+    if(!onvacation) {
+      // fetch lessons count
+      fetch(apiEndpoint('assignments?immediately_available_for_lessons', WKToken))
+          .then(response => response.json())
+          .then(responseBody => {
+              let lessonItemsCount = responseBody.total_count;
 
-    // fetch lessons count
-    fetch(apiEndpoint('assignments?immediately_available_for_lessons', WKToken))
-        .then(response => response.json())
-        .then(responseBody => {
-            let lessonItemsCount = responseBody.total_count;
+              if(lessonItemsCount > 0) {
+                document.getElementById("lesson-items").innerHTML = lessonItemsCount;
+                document.getElementById("lesson-image").style.backgroundColor = "#f100a1";
+              }
+          })
 
-            if(lessonItemsCount > 0) {
-              document.getElementById("lesson-items").innerHTML = lessonItemsCount;
-              document.getElementById("lesson-image").style.backgroundColor = "#f100a1";
-            }
-        })
+      // fetch reviews count
+      fetch(apiEndpoint('assignments?immediately_available_for_review', WKToken))
+          .then(response => response.json())
+          .then(responseBody => {
+              let reviewItemsCount = responseBody.total_count;
 
-    // fetch reviews count
-    fetch(apiEndpoint('assignments?immediately_available_for_review', WKToken))
-        .then(response => response.json())
-        .then(responseBody => {
-            let reviewItemsCount = responseBody.total_count;
-
-            if(reviewItemsCount > 0) {
-              document.getElementById("review-items").innerHTML = reviewItemsCount;
-              document.getElementById("review-image").style.backgroundColor = "#00aaff";
-            }
-        })
+              if(reviewItemsCount > 0) {
+                document.getElementById("review-items").innerHTML = reviewItemsCount;
+                document.getElementById("review-image").style.backgroundColor = "#00aaff";
+              }
+          })
+    }
 
     // fetch username and level
     fetch(apiEndpoint('user', WKToken))
@@ -52,7 +54,11 @@ chrome.storage.sync.get("WKapikey", function(result) {
             let username = responseBody.data.username;
             let level = responseBody.data.level;
 
-            document.getElementById("username").innerHTML = username + " - level " + level + '<span id="options">Settings</span>';
+            if(onvacation) {
+              document.getElementById("username").innerHTML = username + " - currently on vacation" + '<span id="options">Settings</span>';
+            } else {
+              document.getElementById("username").innerHTML = username + " - level " + level + '<span id="options">Settings</span>';
+            }
         })
         .then(() => document.getElementById("options").addEventListener('click', function() {
             chrome.runtime.openOptionsPage();
