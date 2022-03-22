b1897d77c3786f79917faf3041d7e64d4f9c0ba3
Giacomo Carnovalini <giacomo.carnovalini@gmail.com>
2022-03-22 20:01:17 +0000

    added vacation mode to options page

--- options.js
+++ options.js
@@ -2,8 +2,10 @@
 function save_options() {
     let usertoken = document.getElementById('apikey').value;
     let notifylessons = document.getElementById('notifylessons').checked;
+    let onvacation = document.getElementById('vacationmode').checked;
 
     chrome.storage.sync.set({"notifyWKlessons": notifylessons});
+    chrome.storage.sync.set({"vacationModeActive": onvacation});
 
     if(usertoken.length > 0) {
         chrome.storage.sync.set({"WKapikey": usertoken});
@@ -21,6 +23,9 @@ function clear_options() {
     chrome.storage.sync.remove("notifyWKlessons");
     document.getElementById('notifylessons').checked = false;
 
+    chrome.storage.sync.remove("vacationModeActive");
+    document.getElementById('vacationmode').checked = false;
+
     let alert = document.getElementById('alert');
     alert.innerHTML = 'All cleared! - Now add your API token';
 }
@@ -28,9 +33,10 @@ function clear_options() {
 document.getElementById('save').addEventListener('click', save_options);
 document.getElementById('clear').addEventListener('click', clear_options);
 
+
 // Retrieve options from chrome.storage
 function load_options() {
-    chrome.storage.sync.get(["notifyWKlessons", "WKapikey"], function(result) {
+    chrome.storage.sync.get(["notifyWKlessons", "WKapikey", "vacationModeActive"], function(result) {
         if (result.WKapikey != undefined) {
             document.getElementById('apikey').value = result.WKapikey;
         }
@@ -39,9 +45,14 @@ function load_options() {
         if (typeof result.notifyWKlessons === "undefined") {
             chrome.storage.sync.set({"notifyWKlessons": false});
         }
+        if (typeof result.vacationModeActive === "undefined") {
+            chrome.storage.sync.set({"vacationModeActive": false});
+        }
 
         document.getElementById('notifylessons').checked = result.notifyWKlessons;
+        document.getElementById('vacationmode').checked = result.vacationModeActive;
     });
 }
 
 load_options();
+chrome.runtime.sendMessage({do: "check"});
